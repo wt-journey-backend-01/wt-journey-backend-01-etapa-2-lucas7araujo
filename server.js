f7cbae742cf284fbe96739f796484690709ef50b
Lucas Gabriel Adelino Araújo <lucasgabrielz2208@gmail.com>
2025-07-26 11:14:00 -0300

    Documentação Casos - v1

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const app = express();
 const PORT = 3000;
 const casosRoutes = require("./routes/casosRoutes");
 const agentesRoutes = require("./routes/agentesRoutes");
+const swaggerUi = require("swagger-ui-express")
+const swaggerDocs = require("./docs/swagger.json")
 
 app.use(express.json());
 app.use(casosRoutes);
 app.use(agentesRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(PORT, () => {
     console.log(`Servidor do Departamento de Polícia rodando em localhost:${PORT}`);
